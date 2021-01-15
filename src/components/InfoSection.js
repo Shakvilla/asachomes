4a694d0ad349b6dc1cb518bf663368fd17d19623
Shakvilla <abdulshakuraclement@yahoo.com>
2021-01-15 20:03:14 +0000

    added infosection

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.div``
+const Container = styled.div``
+const ColumnLeft = styled.div``
+
 
 function InfoSection() {
     return (
         <div>
 
             <h1>Welcome to AsacHomes</h1>
+
+            <Section>
+                <Container>
+                    <ColumnLeft>
+                        <p>Paragraph</p>
+                        <p>Paragraph</p>
+                        <p>Paragraph</p>
+                        <p>Paragraph</p>
+                        <p>Paragraph</p>
+                        <p>Paragraph</p>
+
+                        <p></p>
+                    </ColumnLeft>
+                </Container>
+            </Section>
         </div>
     )
 }
